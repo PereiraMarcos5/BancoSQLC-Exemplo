8be9e93a466b1bd98c0b4f39edd5d57b437681a1
Your Name <you@example.com>
2019-10-21 16:16:28 -0300

    Commit

---
+++ Form1.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Login
+{
+
+    public partial class Form1 : Form
+    {
+        pro_chefeEntities bd = new pro_chefeEntities();
+        Usuário logado = new Usuário();
+        public Form1()
+        {
+            InitializeComponent();
+            button1.Click += logar;
+        }
+        private void logar(object sender, EventArgs e)
+        {
+            //equivalente a SELECT * FROM usuario
+            /*bd.Usuário.ToList().ForEach(u => {
+                MessageBox.Show(u.nome);*/
+            logado = bd.Usuário.Where(u => u.login.Equals(textBox1.Text)
+            && u.senha.Equals(textBox2.Text)).FirstOrDefault();
+            if (logado != null)
+            {
+                MessageBox.Show($"usuario {logado.nome} logado com sucesso!");
+            }
+            else
+            {
+                MessageBox.Show("Login inválido");
+            }
+        }
+        /*private void cadastrar(object sender, EventArgs e)
+        {
+            new Form2().Show();
+        }*/
+
+        private void Button2_Click(object sender, EventArgs e)
+        {
+            new Form2().Show();
+        }
+    }
+}
---
+++ Form2.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Login
+{
+    public partial class Form2 : Form
+    {
+        pro_chefeEntities bd = new pro_chefeEntities();
+        public Form2()
+        {
+            InitializeComponent();
+            button1.Click += salvar;
+            montarTabela();
+        }
+
+        private void montarTabela()
+        {
+            dataGridView1.DataSource = bd.Usuário.ToList();
+        }
+
+        private void salvar(object sender, EventArgs e)
+        {
+            Usuário novo = new Usuário()
+            {
+                nome = textBox1.Text,
+                login = textBox2.Text,
+                senha = textBox3.Text
+            };
+            //equivalente ao comando INSERT
+            bd.Usuário.Add(novo);
+            bd.SaveChanges();
+            MessageBox.Show("Salvo com Sucesso");
+            montarTabela();
+        }
+
+        private void TextBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
